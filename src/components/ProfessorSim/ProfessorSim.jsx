53350dc45dfcd715dcc915688696a57837132a3f
Rutilusaduro <garbisdk@gmail.com>
2026-06-10 13:23:47 -0400

    Update ProfessorSim.jsx

--- src/components/ProfessorSim/ProfessorSim.jsx
+++ src/components/ProfessorSim/ProfessorSim.jsx
@@ -450,7 +450,7 @@ const ProfessorSim = () => {
               <div style={{
                 position: "absolute",
                 top: "50%",
-                left: "50%",
+                left: "100%",
                 width: "230px",
                 height: "120px",
                 border: "10px solid #9b6dff",
